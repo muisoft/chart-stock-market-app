440a8ce433a99cc87c73b9adb07f7ee242e5d283
muideen <muisoft@github.com>
2018-08-09 22:12:24 +0100

    initial commit

--- src/components/hoc/utils/actionsWrapper/index.js
+++ src/components/hoc/utils/actionsWrapper/index.js
@@ -2,8 +2,8 @@ import io from 'socket.io-client';
 
 //Here we wrap all our actions we wanted to perform on this App
 export const actionsWrapper = (props) => {
-    const url = process.env.NODE_ENV === 'production' ? 'http://localhost:5000' : 'http://localhost:3000';
-    const socket = io.connect('http://localhost:5000');
+    const url = process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000';
+    const socket = io.connect(url);
     return {
         initialData: () => {
             props.initial();
