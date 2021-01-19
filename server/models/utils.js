bb698d25a77d654d0b5d23acc905c6242459e059
muisoft <muisoft2016@gmail.com>
2021-01-19 15:26:27 +0100

    chart bug fixed

--- server/models/utils.js
+++ server/models/utils.js
@@ -3,40 +3,20 @@ const axios = require('axios');
 const Stock = require('mongoose').model('Stock');
 
 module.exports = {
-    // allStocks(done) {
-    //    // console.log("Hello: " + JSON.stringify(stock[0]));
-    //     Stock.find({}, (err, stocks) => {
-    //         if (err) console.error(err);
-    //         let symbols;
-    //         console.log("Check: "+ JSON.stringify(stocks));
-    //         if(stocks){
-    //            symbols = stocks.map(sym => sym.code);
-    //         } else {
-    //             symbols = ["FB"];
-    //         }
-    //         // fetchStock(symbols, (err, stock) => {
-    //         //     if (err) console.error(err);
-    //         //     return done(stock);
-    //         // })
-            
-    //     });
-    //     //console.log("All: "+ JSON.stringify(getDatasets(stock)));
-    //     fetchStock("", (err, stock) => {
-    //                 if (err) console.error(err);
-    //                 return done(stock);
-    //             })
-    // },
     allStocks(done) {
         Stock.find({}, (err, stocks) => {
             if (err) console.error(err);
-            let symbols = stocks.map(sym => sym.code);
+            let symbols = ["FB","AAPL"];
+            if(stocks.length > 0){
+               symbols = stocks.map(sym => sym.code);
+            }
             fetchStock(symbols, (err, stock) => {
                 if (err) console.error(err);
                 return done(stock);
             })
         });
     },
-    saveStock(stockcode, done) {
+    saveStock(stockcode, done) {  
         const code = stockcode;//stockcode.toUppercase();
         fetchStock(code, (err, stock) => {
             if (err) console.error(err);
@@ -56,18 +36,6 @@ module.exports = {
     }
 }
 
-// const fetchStock = (stockCode, done) => {
-   
-//     return done(null, stock);//getDatasets(stock));
-//     // const url = `https://api.iextrading.com/1.0/stock/market/batch?symbols=${stockCode}&types=company,chart&range=1y`;
-//     // const url = 'https://api.iextrading.com/1.0/ref-data/symbols';
-//     const url = `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=AAPL,fb&types=company,chart&range=1m&last=5&&token=Tsk_43e57d4f746143a8a9684a86f0d57fef`;
-//     // axios.get(url)
-//     //     .then(res => {
-//     //         return done(null, getDatasets(res.data));
-//     //     })
-//     //     .catch(err => console.error(err));
-// }
 const fetchStock = (stockCode, done) => {
     // const url = `https://api.iextrading.com/1.0/stock/market/batch?symbols=${stockCode}&types=company,chart&range=1y`;
     const url = `${process.env.BASE_URL}/stable/stock/market/batch?symbols=${stockCode}&types=company,chart&range=1y&token=${process.env.TOKEN}`;
